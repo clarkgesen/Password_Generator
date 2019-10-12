5f11cad3e17e59a2eb8b49b7e75a8552f7c03645
Clark Gesen <cgesen@gmail.com>
2019-10-12 17:27:13 -0400

    Added to readme, commented script.js

--- Develop/script.js
+++ Develop/script.js
@@ -11,6 +11,7 @@ function generatePassword() {
         }
     }
 
+    //prompting for the kind of characters the user would like to include
      var lowerCase = confirm("Would you like to include lower case letters?");
      var upperCase = confirm("Would you like to include upper case letters?");
      var symbols = confirm("Would you like to include symbols?");
@@ -19,20 +20,25 @@ function generatePassword() {
     // Initializing the password variable
     var password = "";
 
-    // Initializing the variable that will be used as an ASCII character code
+    // Initializing the variable that will hold the randomly chosen character
     var character = "";
 
+    // variables that track if a kind of character has been included in the password yet
     var includesSymbols = false;
     var includesLowerCase = false;
     var includesUpperCase = false;
     var includesNumbers = false;
 
     // Used https://stackoverflow.com/questions/1497481/javascript-password-generator as a reference
+    // The case in which the user wants to include all types of characters
     if (lowerCase && upperCase && symbols && numbers) {
-        var charset = ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@%+/\\'!#$^?:,(){}[]~-_.");
+        // setting up a string with all of the valid characters
+        var charset = ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@%+/\\'!#$^?:,(){}[]~-_.`");
         console.log(charset.length)
+        // loops over the charset string, with a random number between 0-85 correlating to a random character in the string
         for (var i = 0; i < length; i++) {
-            var index = Math.floor(Math.random() * 85);
+            var index = Math.floor(Math.random() * 86);
+            // Checking to see what kind of character has been chosen
             if (index < 26) {
                 includesLowerCase = true;
             } else if (index > 25 && index < 52) {
@@ -43,6 +49,7 @@ function generatePassword() {
                 includesSymbols = true;
             }
 
+            // If a type of character hasn't been chosen for the password yet, the random number index will be narrowed to only include that character type
             if (i === (length - 4) && includesLowerCase === false) {
                 index = Math.floor(Math.random() * 26);
                 console.log(character)
@@ -51,19 +58,21 @@ function generatePassword() {
             } else if (i === (length - 2) && includesNumbers === false) {
                 index = 52 + Math.floor(Math.random() * 10);
             } else if (i === (length - 1) && includesSymbols === false) {
-                index = 62 + Math.floor(Math.random() * 23);
+                index = 62 + Math.floor(Math.random() * 24);
             } else {
                 console.log(index)
             }
 
-
+            // using the random index to choose the random character
             character = charset.charAt(index);
+            // concatenating the character onto the password
             password = password + character;
         }
+    // running through the same process as above, but without the numbers in the charset
     } else if (lowerCase && upperCase && symbols && !numbers) {
-        var charset = ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@%+/'!#$^?:,(){}[]~-_.");
+        var charset = ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@%+/\\'!#$^?:,(){}[]~-_.`");
         for (var i = 0; i < length; i++) {
-            var index = Math.floor(Math.random() * 75);
+            var index = Math.floor(Math.random() * 76);
 
             if (index < 26) {
                 includesLowerCase = true
@@ -78,13 +87,14 @@ function generatePassword() {
             } else if (i == length - 2 && includesUpperCase == false) {
                 index = 26 + Math.floor(Math.random() * 26);
             } else if (i == length - 1 && includesSymbols == false) {
-                index = 52 + Math.floor(Math.random() * 23);
+                index = 52 + Math.floor(Math.random() * 24);
             }
 
             character = charset.charAt(index);
             password = password + character;
 
-            } 
+            } \
+    // Charset without symbols
     } else if (lowerCase && upperCase && !symbols && numbers) {
             var charset = ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890");
             for (var i = 0; i < length; i++) {
@@ -109,26 +119,28 @@ function generatePassword() {
                 character = charset.charAt(index);
                 password = password + character;
             }
+        // Charset without upper case
         } else if (lowerCase && !upperCase && symbols && numbers) {
-            var charset = ("abcdefghijklmnopqrstuvwxyz1234567890@%+/\'!#$^?:,(){}[]~-_.");
+            var charset = ("abcdefghijklmnopqrstuvwxyz1234567890@%+/\\'!#$^?:,(){}[]~-_.`");
             for (var i = 0; i < length; i++) {
-                var index = Math.floor(Math.random() * 59);
+                var index = Math.floor(Math.random() * 60);
 
                 if (i == length - 3 && includesLowerCase == false) {
                     index = Math.floor(Math.random() * 26);
                 } else if (i == length - 2 && includesNumbers == false) {
                     index = 26 + Math.floor(Math.random() * 10);
                 } else if (i == length - 1 && includesSymbols == false) {
-                    index = 36 + Math.floor(Math.random() * 23);
+                    index = 36 + Math.floor(Math.random() * 24);
                 }
 
                 character = charset.charAt(index);
                 password = password + character;
             }
+        // Charset without lower case letters
         } else if (!lowerCase && upperCase && symbols && numbers) {
-            var charset = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@%+/\'!#$^?:,(){}[]~-_.");
+            var charset = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@%+/\\'!#$^?:,(){}[]~-_.`");
             for (var i = 0; i < length; i++) {
-                var index = Math.floor(Math.random() * 59);
+                var index = Math.floor(Math.random() * 60);
 
                 if (index < 26) {
                     includesUpperCase = true;
@@ -143,12 +155,13 @@ function generatePassword() {
                 } else if (i == length - 2 && includesNumbers == false) {
                     index = 26 + Math.floor(Math.random() * 10);
                 } else if (i == length - 1 && includesSymbols == false) {
-                    index = 36 + Math.floor(Math.random() * 23);
+                    index = 36 + Math.floor(Math.random() * 24);
                 }
 
                 character = charset.charAt(index);
                 password = password + character;
             }
+        // Charset without symbols or numbers
         } else if (lowerCase && upperCase && !symbols && !numbers) {
             var charset = ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
             for (var i = 0; i < length; i++) {
@@ -169,10 +182,11 @@ function generatePassword() {
                 character = charset.charAt(index);
                 password = password + character;
             }
+        // Charset without upper case letters and numbers
         } else if (lowerCase && !upperCase && symbols && !numbers) {
-            var charset = ("abcdefghijklmnopqrstuvwxyz@%+/\'!#$^?:,(){}[]~-_.");
+            var charset = ("abcdefghijklmnopqrstuvwxyz@%+/\\'!#$^?:,(){}[]~-_.`");
             for (var i = 0; i < length; i++) {
-                var index = Math.floor(Math.random() * 49);
+                var index = Math.floor(Math.random() * 50);
 
                 if (index < 26) {
                     includesLowerCase = true;
@@ -183,16 +197,17 @@ function generatePassword() {
                 if (i == length - 2 && includesLowerCase == false) {
                     index = Math.floor(Math.random() * 26)
                 } else if (i == length - 1 && includesSymbols == false) {
-                    index = 26 + Math.floor(Math.random() * 23);
+                    index = 26 + Math.floor(Math.random() * 24);
                 }
 
                 character = charset.charAt(index);
                 password = password + character;
             }
+        // Charset without lower case letters or numbers
         } else if (!lowerCase && upperCase && symbols && !numbers) {
-            var charset = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ@%+/\'!#$^?:,(){}[]~-_.");
+            var charset = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ@%+/\\'!#$^?:,(){}[]~-_.`");
             for (var i = 0; i < length; i++) {
-                var index = Math.floor(Math.random() * 49);
+                var index = Math.floor(Math.random() * 50);
 
                 if (index < 26) {
                     includesUpperCase = true;
@@ -203,12 +218,13 @@ function generatePassword() {
                 if (i == length - 2 && includesUpperCase == false) {
                     index = Math.floor(Math.random() * 26);
                 } else if (i == length - 1 && includesSymbols == false) {
-                    index = 26 + Math.floor(Math.random() * 23);
+                    index = 26 + Math.floor(Math.random() * 24);
                 }
 
                 character = charset.charAt(index);
                 password = password + character;
             }
+        // Charset without lower case letters or symbols
         } else if (!lowerCase && upperCase && !symbols && numbers) {
             var charset = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890");
             for (var i = 0; i < length; i++) {
@@ -229,10 +245,11 @@ function generatePassword() {
                 character = charset.charAt(index);
                 password = password + character;
             }
+        // Charset without upper case or lower case letters
         } else if (!lowerCase && !upperCase && symbols && numbers) {
-            var charset = ("1234567890@%+/\'!#$^?:,(){}[]~-_.");
+            var charset = ("1234567890@%+/\\'!#$^?:,(){}[]~-_.`");
             for (var i = 0; i < length; i++) {
-                var index = Math.floor(Math.random() * 33);
+                var index = Math.floor(Math.random() * 34);
 
                 if (index < 10) {
                     includesNumbers = true;
@@ -243,12 +260,13 @@ function generatePassword() {
                 if (i == length - 2 && includesNumbers == false) {
                     index = Math.floor(Math.random() * 10);
                 } else if (i == length - 1 && includesSymbols == false) {
-                    index = 10 + Math.floor(Math.random() * 23);
+                    index = 10 + Math.floor(Math.random() * 24);
                 }
 
                 character = charset.charAt(index);
                 password = password + character;
             }
+        // Charset without upper case letters or symbols
         } else if (lowerCase && !upperCase && !symbols && numbers) {
             var charset = ("abcdefghijklmnopqrstuvwxyz1234567890");
             for (var i = 0; i < length; i++) {
@@ -269,6 +287,7 @@ function generatePassword() {
                 character = charset.charAt(index);
                 password = password + character;
             }
+        // Charset that only includes numbers
         } else if (!lowerCase && !upperCase && !symbols && numbers) {
             var charset = ("1234567890");
             for (var i = 0; i < length; i++) {
@@ -276,13 +295,15 @@ function generatePassword() {
                 character = charset.charAt(index);
                 password = password + character;
             }
+        // Charset that only includes symbols
         } else if (!lowerCase && !upperCase && symbols && !numbers) {
-            var charset = ("@%+/\'!#$^?:,(){}[]~-_.");
+            var charset = ("@%+/\\'!#$^?:,(){}[]~-_.`");
             for (var i = 0; i < length; i++) {
-                var index = Math.floor(Math.random() * 23);
+                var index = Math.floor(Math.random() * 24);
                 character = charset.charAt(index);
                 password = password + character;
             }
+        // Charset that only includes upper case letters
         } else if (!lowerCase && upperCase && !symbols && !numbers) {
             var charset = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
             for (var i = 0; i < length; i++) {
@@ -290,6 +311,7 @@ function generatePassword() {
                 character = charset.charAt(index);
                 password = password + character;
             }
+        // Charset that only includes lower case letters
         } else if (lowerCase && !upperCase && !symbols && !numbers) {
             var charset = ("abcdefghijklmnopqrstuvwxyz");
             for (var i = 0; i < length; i++) {
@@ -297,18 +319,20 @@ function generatePassword() {
                 character = charset.charAt(index);
                 password = password + character;
             }
+        // If none of the character types are chosen, an alert will be shown
         } else {
             alert("Please allow at least one type of character")
         }
          
         console.log(password);
-
+        // Displaying the password to the text area
         var passwordDisplay = document.querySelector("#password");
+        console.log(passwordDisplay);
 
-        passwordDisplay.setAttribute("placeholder", password);
+        passwordDisplay.value = password;
     }
     
-    
+    // Function to copy the password to the clipboard
     function copyToClipboard() {
         document.getElementById("password").select();
         document.execCommand("copy");
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Password_Generator
 A random password generator that lets you choose length and type of characters.
+
+## Password Options
+The first option for your password is the length, any number between 8 and 128 is valid. 
+You can also choose what kind of characters you would like in your password. The program will prompt you to chose which kinds of characters you would like included in your password; the prompt will ask if you would like to include lower case letters, upper case letters, special symbols, and numbers. Each password is guaranteed to have at least one character from each kind you select.
+
+## Copy to Clipboard
+After your password has been generated, you have the option to copy it to your clipboard
